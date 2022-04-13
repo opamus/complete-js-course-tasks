267a60083c52c0f6bef9f2a290828d32c9cddc9b
Olli Lehmusvuori <ollilehmusvuori@gmail.com>
2022-04-13 18:55:46 +0300

    Move some repeated code to variables

--- CoinFlip/script.js
+++ CoinFlip/script.js
@@ -7,18 +7,22 @@ const flipCoin = () => {
   const coin = document.getElementById("Coin");
   const value = Math.floor(Math.random() * 2);
 
+  const resultText = `Coin was flipped, you got ${headsOrTails.innerText}`;
+  const delayTime = 1900;
+
   if (value === 1) {
     coin.style.animation = "flip-heads 2s forwards";
     setTimeout(() => {
       headsOrTails.innerText = "Heads";
-    }, 1800);
+      console.log(resultText);
+    }, delayTime);
   } else {
     coin.style.animation = "flip-tails 2s forwards";
     setTimeout(() => {
       headsOrTails.innerText = "Tails";
-    }, 1800);
+      console.log(resultText);
+    }, delayTime);
   }
-  console.log(`Coin was flipped, you got ${headsOrTails.innerText}`);
 };
 
 coinContainer.addEventListener("click", flipCoin);
